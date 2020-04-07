ae918e6603e9b13be548eaa6e4c49b8fb507b2d7
teomandi <teomandi@gmail.com>
2020-04-07 23:42:32 +0300

    first commit

---
+++ app/src/main/java/com/example/frontmynbnb/RegisterActivity.java
@@ -0,0 +1,42 @@
+package com.example.frontmynbnb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+    EditText mTextUsername;
+    EditText mTextPassword;
+    EditText mTextCnfPassword;
+    Button mButtonRegister;
+    TextView mTextViewLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mTextUsername = (EditText) findViewById(R.id.edittext_username);
+        mTextPassword = (EditText) findViewById(R.id.edittext_password);
+        mTextCnfPassword = (EditText) findViewById(R.id.edittext_cnf_password);
+
+        mButtonRegister = (Button) findViewById(R.id.button_register);
+        mTextViewLogin = (TextView) findViewById(R.id.textview_login);
+        mTextViewLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(
+                        RegisterActivity.this,
+                        LoginActivity.class
+                );
+                startActivity(registerIntent);
+            }
+        });
+
+    }
+}
