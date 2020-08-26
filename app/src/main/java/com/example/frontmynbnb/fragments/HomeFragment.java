4ed7c0e113dce8c7d28adc1ead330425a6fa96ca
teomandi <teomandi@gmail.com>
2020-08-26 23:26:41 +0300

    minimize and expanig functionality added. also some styling

--- app/src/main/java/com/example/frontmynbnb/fragments/HomeFragment.java
+++ app/src/main/java/com/example/frontmynbnb/fragments/HomeFragment.java
@@ -10,6 +10,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -23,9 +25,12 @@ import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
-    private EditText mEditSearch;
-    LinearLayout targetView;
-    Button miniButton, expandButton;
+    private EditText mEditSearch, mEditNumPeople;
+    private LinearLayout mProgressView, mSearchGroup, mBackground;
+    private TextView mTextOnProgressBar;
+    private RadioGroup mRadioType;
+    private Button mButtonFrom, mButtonTo, mButtonSearch;
+
 
     private ListView placesContainer;
     private ArrayList<Place> placeList;
@@ -35,26 +40,34 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        mEditSearch = (EditText) view.findViewById(R.id.edittext_search);
-        targetView = (LinearLayout) view.findViewById(R.id.moving_view);
 
-        expandButton = (Button) view.findViewById(R.id.expand_button);
-        expandButton.setOnClickListener(new View.OnClickListener() {
+        mEditSearch = (EditText) view.findViewById(R.id.edittext_search);
+        mEditNumPeople = (EditText) view.findViewById(R.id.edittext_numofpeople);
+        mProgressView = (LinearLayout) view.findViewById(R.id.layout_progresscomponent2);
+        mSearchGroup = (LinearLayout) view.findViewById(R.id.layout_searchview);
+        mBackground = (LinearLayout) view.findViewById(R.id.layout_background);
+        mTextOnProgressBar = (TextView) view.findViewById(R.id.text_on_bar3);
+        mRadioType = (RadioGroup) view.findViewById(R.id.radio_typesearch);
+        mButtonFrom = (Button) view.findViewById(R.id.button_selectfrom);
+        mButtonTo = (Button) view.findViewById(R.id.button_selectto);
+        mButtonSearch = (Button) view.findViewById(R.id.button_searchplaces);
+
+        mEditSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(targetView.getVisibility() == View.GONE){
-                    TransitionManager.beginDelayedTransition(targetView, new AutoTransition());
-                    targetView.setVisibility(View.VISIBLE);
+                if(mSearchGroup.getVisibility() == View.GONE){
+                    TransitionManager.beginDelayedTransition(mSearchGroup, new AutoTransition());
+                    mSearchGroup.setVisibility(View.VISIBLE);
                 }
             }
         });
-        miniButton = (Button) view.findViewById(R.id.minimaze_button);
-        miniButton.setOnClickListener(new View.OnClickListener() {
+        //if clicked somewhere else then minimize it
+        mBackground.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(targetView.getVisibility() == View.VISIBLE){
-                    TransitionManager.beginDelayedTransition(targetView, new AutoTransition());
-                    targetView.setVisibility(View.GONE);
+                if(mSearchGroup.getVisibility() == View.VISIBLE){
+                    TransitionManager.beginDelayedTransition(mSearchGroup, new AutoTransition());
+                    mSearchGroup.setVisibility(View.GONE);
                 }
             }
         });
