98e64ba9cea68ca47ed49460ef29699cad70dec5
teomandi <teomandi@gmail.com>
2020-08-26 22:56:06 +0300

    view for expanded search

--- app/src/main/java/com/example/frontmynbnb/fragments/HomeFragment.java
+++ app/src/main/java/com/example/frontmynbnb/fragments/HomeFragment.java
@@ -1,10 +1,14 @@
 package com.example.frontmynbnb.fragments;
 
 import android.os.Bundle;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -19,6 +23,10 @@ import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
+    private EditText mEditSearch;
+    LinearLayout targetView;
+    Button miniButton, expandButton;
+
     private ListView placesContainer;
     private ArrayList<Place> placeList;
 
@@ -27,6 +35,29 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        mEditSearch = (EditText) view.findViewById(R.id.edittext_search);
+        targetView = (LinearLayout) view.findViewById(R.id.moving_view);
+
+        expandButton = (Button) view.findViewById(R.id.expand_button);
+        expandButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(targetView.getVisibility() == View.GONE){
+                    TransitionManager.beginDelayedTransition(targetView, new AutoTransition());
+                    targetView.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        miniButton = (Button) view.findViewById(R.id.minimaze_button);
+        miniButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(targetView.getVisibility() == View.VISIBLE){
+                    TransitionManager.beginDelayedTransition(targetView, new AutoTransition());
+                    targetView.setVisibility(View.GONE);
+                }
+            }
+        });
 
 
         return view;
